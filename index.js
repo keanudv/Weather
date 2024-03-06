e30de9d2938872f609d94c2974673861783f7456
Keanu Valencia <104657054+keanudv@users.noreply.github.com>
2024-03-06 07:04:53 +0000

    Updated to handle latitude and longitude.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
-Description: Takes a city name from the client and returns weather data from an API.
+Description: Takes latitue and longitude from the client and returns weather data from an API based on the client's input.
 
 Name: Keanu Valencia
-Date: 2/28/24
+Date: 3/5/24
 Class: ICS 385
 **/
 
@@ -14,23 +14,25 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-//displays index.html of root path.
+// displays index.html of root path.
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html")
 });
 
-//invoked after hitting go in the html form.
+// invoked after hitting go in the html form.
 app.post("/", function(req, res) {
     
-    // takes in the city from the html form and console logs it.
-    var cityName = String(req.body.cityInput);
-    console.log(req.body.cityInput);
-    
+    // takes in the Lat and Long from the client and stores them in a String variable.
+    var lat = String(req.body.latInput);
+    console.log(req.body.latInput);
+    var long = String(req.body.longInput);
+    console.log(req.body.longInput);
+
     const units = "imperial";
     const apiKey = "e7dfd634660c6044b98fa6f22b0dd6a1";
 
-    //API URL with parameters.
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=" + units + "&APPID=" + apiKey;
+    // API URL with parameters.
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=" + units + "&appid=" + apiKey;
     
     // this gets the data from Open WeatherAPI.
     https.get(url, function(response){
@@ -41,11 +43,13 @@ app.post("/", function(req, res) {
           const weatherData = JSON.parse(data);
 
           console.log(weatherData);
-          
+
+          // Variables for Description, Temp, Icon, Humidity, Wind Speed, and Cloudiness
           const temp = weatherData.main.temp;
           const city = weatherData.name;
           const humidity = weatherData.main.humidity;
           const windSpeed = weatherData.wind.speed;
+          const cloudiness = weatherData.clouds.all;
           const weatherDescription = weatherData.weather[0].description;
           const icon = weatherData.weather[0].icon;
           const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
@@ -55,19 +59,20 @@ app.post("/", function(req, res) {
           res.write("<h2>The temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");
           res.write("<h2>The humidity in " + city + " is " + humidity + "% </h2>");
           res.write("<h2> The winds in " + city + " are " + windSpeed + " miles per hour </h2>");
+          res.write("<h2> The cloudiness in " + city + " is " + cloudiness + "% </h2>");
           res.write("<img src=" + imageURL +">");
           res.send();
         });
     });
 })
 
-// Output on the termial.
+// Output on the terminal.
 app.listen(process.env.PORT || 3000, function() {
 console.log ("Server is running on port")
 });
 
 /**
-Test Scripts: Enter different city names and press the Go button to see if it works.
+Test Scripts: See if the submit button returns all required data from the API.
 
 AI used and Prompt: None.
 **/
