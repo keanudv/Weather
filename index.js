e2f21381ec8d911f6c270654947f90260ce2f568
Keanu Valencia <104657054+keanudv@users.noreply.github.com>
2024-03-13 03:38:52 +0000

    Exam 2

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/**
-Description: Takes latitue and longitude from the client and returns weather data from an API based on the client's input.
-
-Name: Keanu Valencia
-Date: 3/5/24
-Class: ICS 385
-**/
-
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
@@ -14,65 +6,57 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-// displays index.html of root path.
+//displays index.html of root path
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html")
 });
 
-// invoked after hitting go in the html form.
+//invoked after hitting go in the html form
 app.post("/", function(req, res) {
-    
-    // takes in the Lat and Long from the client and stores them in a String variable.
-    var lat = String(req.body.latInput);
-    console.log(req.body.latInput);
-    var long = String(req.body.longInput);
-    console.log(req.body.longInput);
 
-    const units = "imperial";
-    const apiKey = "e7dfd634660c6044b98fa6f22b0dd6a1";
+    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
+        var city_id = String(req.body.cityInput);
+        console.log(req.body.cityInput);
 
-    // API URL with parameters.
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=" + units + "&appid=" + apiKey;
-    
-    // this gets the data from Open WeatherAPI.
+    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
+        const units = "imperial";
+        const apiKey = "e7dfd634660c6044b98fa6f22b0dd6a1";
+
+          const url = "https://api.openweathermap.org/data/2.5/weather?id=" + city_id + "&units=" + units + "&appid=" + apiKey;
+
+           
+          //"https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+
+    // this gets the data from Open WeatherPI
     https.get(url, function(response){
         console.log(response.statusCode);
-        
-        // Creating variables based on the data from the API.
+
+        // gets individual items from Open Weather API
         response.on("data", function(data){
-          const weatherData = JSON.parse(data);
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const city = weatherData.name;
+            const humidity = weatherData.main.humidity;
+            const windSpeed = weatherData.wind.speed;
+            const winddirection = weatherData.wind.deg;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-          console.log(weatherData);
-
-          // Variables for Description, Temp, Icon, Humidity, Wind Speed, and Cloudiness
-          const temp = weatherData.main.temp;
-          const city = weatherData.name;
-          const humidity = weatherData.main.humidity;
-          const windSpeed = weatherData.wind.speed;
-          const cloudiness = weatherData.clouds.all;
-          const weatherDescription = weatherData.weather[0].description;
-          const icon = weatherData.weather[0].icon;
-          const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            
-          // displays the output of the results.
-          res.write("<h1> The weather is " + weatherDescription + "<h1>");
-          res.write("<h2>The temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");
-          res.write("<h2>The humidity in " + city + " is " + humidity + "% </h2>");
-          res.write("<h2> The winds in " + city + " are " + windSpeed + " miles per hour </h2>");
-          res.write("<h2> The cloudiness in " + city + " is " + cloudiness + "% </h2>");
-          res.write("<img src=" + imageURL +">");
-          res.send();
+            // displays the output of the results
+            res.write("<h1> The weather is " + weatherDescription + "<h1>");
+            res.write("<h2>The Temperature in " + city + " " + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write(humidity)
+            res.write(windSpeed)
+            res.write(winddirection)
+            res.write("<img src=" + imageURL +">");
+            res.send();
         });
     });
 })
 
-// Output on the terminal.
+
+//Code will run on 3000 or any available open port
 app.listen(process.env.PORT || 3000, function() {
 console.log ("Server is running on port")
 });
-
-/**
-Test Scripts: See if the submit button returns all required data from the API.
-
-AI used and Prompt: None.
-**/
